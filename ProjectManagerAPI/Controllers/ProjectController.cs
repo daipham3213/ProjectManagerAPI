cdb062ff72c7492516eb7feeef9714c086836b2d
PhamVinhPhat <pvphat12c6ntt@gmail.com>
2021-06-24 17:21:34 +0700

    Project List

---
+++ ProjectManagerAPI/Controllers/ProjectController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjectManagerAPI.Core;
+using ProjectManagerAPI.Core.Repositories;
+using System.Threading.Tasks;
+
+
+namespace ProjectManagerAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectController : ControllerBase
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProjectController(IUnitOfWork unitOfWork) {
+            this._unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll(string postId)
+        {
+            var projects = await _unitOfWork.Project.GetAll();
+            return Ok(projects);
+        }
+
+
+    }
+}
---
+++ ProjectManagerAPI/Core/Repositories/IProjectRepository.cs
@@ -0,0 +1,14 @@
+﻿using ProjectManagerAPI.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProjectManagerAPI.Core.Repositories
+{
+    public interface IProjectRepository : IRepository<Project>
+    {
+        public void LoadParent(Project project);
+        public void RemoveRelation(Project project);
+    }
+}
